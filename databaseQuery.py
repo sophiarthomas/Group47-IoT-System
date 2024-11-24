2e2c1039c4151dd7161cc69050812776fb5cb9c3
Sophia Thomas <sophiart03@gmail.com>
2024-11-23 20:57:39 -0800

    Add if-conditions to main()
    In order to call the correct query function

--- databaseQuery.py
+++ databaseQuery.py
@@ -2,9 +2,12 @@ from pymongo.mongo_client import MongoClient
 from dotenv import load_dotenv
 import os
 import mongoDBkey as key
+from datetime import datetime, timedelta
 
 # Query 1: What is the average moisture inside my kitchen fridge in the past three hours?
 def fridge_moisture(): 
+    # Get the date for 3 hours ago
+    three_hours_ago = datetime.now() - timedelta(hours=3)
     pass 
 
 
@@ -18,7 +21,7 @@ def electricity_consumption():
     pass 
 
 
-def main(): 
+def main(msg): 
     # Load environment variables from .env file
     load_dotenv()
 
@@ -41,6 +44,13 @@ def main():
     query = {"customAttributes.name": "Smart Fridge"}
     document = metadata.find_one(query)
     print(document)
+
+    if msg == "What is the average moisture inside my kitchen fride in the past three hours?": 
+        fridge_moisture()
+    elif msg == "What is the average water consumption per cycle in my smart dishwasher?":
+        avg_water_consumption()
+    elif msg == "What device consumed more electriciy among my three IoT devices (two refrigerators and a dishwasher)?":
+        electricity_consumption()
 
 
 if __name__=="__main__":
