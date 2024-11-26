186cbe9048fd7bf861b212644ccdcdfefd78ed20
Peter Khim <peter.khim01@student.csulb.edu>
2024-11-26 02:36:05 -0800

    Added skeleton server setup with query stuff

--- server.py
+++ server.py
@@ -5,36 +5,83 @@
 
 import socket
 import ipaddress
-import pymongo import MongoClient
+from pymongo import MongoClient
 
 client = MongoClient('mongodb+srv://peterkhim:eucalyptus@smartdevices.a2nsj.mongodb.net/')
 db = client['test']
 data_collection = db['IoTSmartDevices_virtual']
 metadata_collection = db['IoTSmartDevices_metadata']
 
+def get_device_info(device_id):		# This way we can tell what device connects with what sensors.
+	device = metadata_collection.find_one({" ": device_id}) # " " is parentID or uID, need to figure out which one we want.
+	if device:
+		return device
+	return None
 
-serverIP = input("Enter the server ip address: ")
-serverPort = int(input("Enter port: "))
+def calculate_average_moisture(device_id):  # Query 1
+	pass
 
-## creates listening socket (IPv4, tcp socket stream)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcpSock: 
-    tcpSock.bind((serverIP, serverPort)) # binds the created socket to the host and port you want to listen to 
-    print(f"binding: {serverIP}:{serverPort}")
-    tcpSock.listen() # listen to a request for the server from this socket
-    print("Server is listening for connections...")
+def calculate_average_water_consumption(device_id): # Query 2
+	pass
 
-    while True:
-        incomingSocket, clientAddr = tcpSock.accept() 
-        with incomingSocket: 
-            print(f"Connected by {clientAddr}")
-            while True: 
-                data = incomingSocket.recv(1024) # Recieve data from client 
-                if not data: 
-                    print("Connection Lost")
-                    print("Server is listening for new connections...")
-                    break
-                print(f"Recieved: {data.decode('utf-8')}")
-                incomingSocket.sendall(data.upper()) # Send response to the client 
-                print(f"Sent: {data.upper().decode('utf-8')}")
+def compare_device_power(): # Query 3
+	pass
+
+def handle_query(query):
+	if "average moisture" in query.lower():
+		return calculate_average_moisture("kitchen_fridge")
+	elif "average water consumption" in query.lower():
+		return calculate_average_water_consumption("dishwasher")
+	elif "device consumed more electricty" in query.lower():
+		return compare_device_power()
+	else:
+		return "Sorry, this query cannot be processed. Please try one of the following: [LIST OF VALID QUERIES, THIS MUST BE CHANGED BUT LAZY RN!!!!!!]"
+
+def main():
+	myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	while True:
+		try:
+		   server_ip = input("Enter the server IP address: ")
+		   ipaddress.ip_address(server_ip)
+		   break
+		except ValueError:
+			print("Invalid IP Address. Try Again.")
+
+	while True:
+		try:
+		   server_port = int(input("Enter the server port number: "))
+		   if 1 <= server_port <= 65535:
+			break
+		   else:
+			print("Port number must be between 1 and 65535. Try Again.")
+		except ValueError:
+		   print("Port number must be an integer. Try Again.")
 	
-        
+	myTCPSocket.bind((server_ip, server_port))
+	print(f"Server up on {server_ip}:{server_port}.")
+
+	myTCPSocket.listen(1)
+
+	while True:
+		print("Waiting for connection.")
+		incomingSocket, incomingAddress = myTCPSocket.accept()
+		print(f"Connected to {incomingAddress}")
+
+		while True:
+			data = incomingSocket.recv(1024).decode('utf-8')
+			if not data:
+				break
+
+		print(f"Recieved query: {data}")
+
+		response = handle_query(data)
+
+		incomingSocket.send(response.encode('utf-8'))
+		print(f"Sent respose: {response}")
+
+	incomingSocket.close()
+	print(f"Connection with {incomingAddress} terminated.")
+
+if __name__ == "__main__":
+	main()
